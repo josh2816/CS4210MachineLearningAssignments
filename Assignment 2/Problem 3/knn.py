12d6772a99d2b499b20a4c221dec3486a727bb10
josh2816 <164451084+josh2816@users.noreply.github.com>
2025-09-29 16:24:30 -0700

    k

--- Assignment 2/Problem 3/knn.py
+++ Assignment 2/Problem 3/knn.py
@@ -11,46 +11,53 @@
 #Importing some Python libraries
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
-
+import numpy as np
 #Reading the data in a csv file using pandas
-db = []
 df = pd.read_csv('email_classification.csv')
-for _, row in df.iterrows():
-    db.append(row.tolist())
+#print(df)
+df.iloc[:, -1] = df.iloc[:, -1].str.strip().str.lower().map({'ham': 0.0, 'spam': 1.0})
+#print(df)
 
+X=[]
+Y=[]
+class_predicted = []
+num_errors = 0
 #Loop your data to allow each instance to be your test set
-for i in db:
-
-    #Add the training features to the 20D array X removing the instance that will be used for testing in this iteration.
+for i in df.index:
+    pass
+    #Add the training features to the 2D array X removing the instance that will be used for testing in this iteration.
     #For instance, X = [[1, 2, 3, 4, 5, ..., 20]].
     #Convert each feature value to float to avoid warning messages
     #--> add your Python code here
+    X = df.drop(i).iloc[:, :-1].astype(float).values.tolist()
 
     #Transform the original training classes to numbers and add them to the vector Y.
     #Do not forget to remove the instance that will be used for testing in this iteration.
     #For instance, Y = [1, 2, ,...].
     #Convert each feature value to float to avoid warning messages
     #--> add your Python code here
+    Y = df.drop(i).iloc[:, -1].values.tolist()
 
     #Store the test sample of this iteration in the vector testSample
     #--> add your Python code here
+    test_sample = df.iloc[i, :-1].astype(float).values.tolist()
 
     #Fitting the knn to the data using k = 1 and Euclidean distance (L2 norm)
     #--> add your Python code here
-    clf =
+    clf = KNeighborsClassifier(n_neighbors=1, metric='euclidean')
+    clf.fit(X, Y)
 
     #Use your test sample in this iteration to make the class prediction. For instance:
     #class_predicted = clf.predict([[1, 2, 3, 4, 5, ..., 20]])[0]
     #--> add your Python code here
+    class_predicted = clf.predict([test_sample])[0]
 
     #Compare the prediction with the true label of the test instance to start calculating the error rate.
     #--> add your Python code here
-
+    if class_predicted == df.iloc[i, -1]:
+        num_errors +=1
+#print(X)
+#print(Y)
 #Print the error rate
 #--> add your Python code here
-
-
-
-
-
-
+print("Error rate:", num_errors/len(df))
