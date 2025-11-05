3147d2dca9cc9b75e416e200a15abb01c6509b28
josh2816 <164451084+josh2816@users.noreply.github.com>
2025-11-05 13:22:17 -0800

    aa

--- Assignment 3/perceptron.py
+++ Assignment 3/perceptron.py
@@ -14,8 +14,8 @@ from sklearn.neural_network import MLPClassifier #pip install scikit-learn==0.18
 import numpy as np
 import pandas as pd
 
-n = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]
-r = [True, False]
+n = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]   #learning rate
+r = [True, False]       #shuffle flag
 
 df = pd.read_csv('optdigits.tra', sep=',', header=None) #reading the data by using Pandas library
 
@@ -27,20 +27,23 @@ df = pd.read_csv('optdigits.tes', sep=',', header=None) #reading the data by usi
 X_test = np.array(df.values)[:,:64]    #getting the first 64 fields to form the feature data for test
 y_test = np.array(df.values)[:,-1]     #getting the last field to form the class label for test
 
-for : #iterates over n
+highestPerceptronAccuracy = -999999
+highestMLPAccuracy = -999999
+for i in n: #iterates over n
 
-    for : #iterates over r
+    for j in r: #iterates over r
 
         #iterates over both algorithms
         #-->add your Python code here
 
-        for : #iterates over the algorithms
+        for algorithm in ["Perceptron", "MLP"]: #iterates over the algorithms
 
             #Create a Neural Network classifier
-            #if Perceptron then
-            #   clf = Perceptron()    #use those hyperparameters: eta0 = learning rate, shuffle = shuffle the training data, max_iter=1000
-            #else:
-            #   clf = MLPClassifier() #use those hyperparameters: activation='logistic', learning_rate_init = learning rate,
+            if algorithm == "Perceptron":
+               clf = Perceptron(eta0 = i, shuffle = j, max_iter = 1000)    #use those hyperparameters: eta0 = learning rate, shuffle = shuffle the training data, max_iter=1000
+            else:
+               clf = MLPClassifier(activation = 'logistic', learning_rate_init=i,
+                                   hidden_layer_sizes=(25), shuffle = j, max_iter=1000) #use those hyperparameters: activation='logistic', learning_rate_init = learning rate,
             #                          hidden_layer_sizes = number of neurons in the ith hidden layer - use 1 hidden layer with 25 neurons,
             #                          shuffle = shuffle the training data, max_iter=1000
             #-->add your Python code here
@@ -50,23 +53,22 @@ for : #iterates over n
 
             #make the classifier prediction for each test sample and start computing its accuracy
             #hint: to iterate over two collections simultaneously with zip() Example:
-            #for (x_testSample, y_testSample) in zip(X_test, y_test):
-            #to make a prediction do: clf.predict([x_testSample])
-            #--> add your Python code here
+            correct = 0
+            total = 0
+            accuracy = 0
+            for (x_testSample, y_testSample) in zip(X_test, y_test):
+                if (clf.predict([x_testSample]) == y_testSample):
+                    correct+=1
+                total+=1
+            accuracy = correct/total
 
             #check if the calculated accuracy is higher than the previously one calculated for each classifier. If so, update the highest accuracy
             #and print it together with the network hyperparameters
             #Example: "Highest Perceptron accuracy so far: 0.88, Parameters: learning rate=0.01, shuffle=True"
             #Example: "Highest MLP accuracy so far: 0.90, Parameters: learning rate=0.02, shuffle=False"
-            #--> add your Python code here
-
-
-
-
-
-
-
-
-
-
-
+            if algorithm == "Perceptron" and highestPerceptronAccuracy < accuracy:
+                highestPerceptronAccuracy = accuracy
+                print(f"Highest Perceptron Accuracy so far: {accuracy}, Parameters: learning rate = {i}, shuffle = {j}")
+            elif algorithm == "MLP" and highestMLPAccuracy < accuracy:
+                highestMLPAccuracy = accuracy
+                print(f"Highest MLP Accuracy so far: {accuracy}, Parameters: learning rate = {i}, shuffle = {j}")
